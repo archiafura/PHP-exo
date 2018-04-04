62990f26d1c0a1cb70350670af55a0e96ee31caf
Isa Vil <isa.vil@free.fr>
2018-04-04 16:44:42 +0200

    ok

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
 <footer>
-<p>&copy; <?php echo date('Y'); ?>  - Dieppe Méningo Corp </p>
+<p>&copy; <?php echo date('Y'); ?>  - Dieppe Méningo <abbr title= "corporation">Corp.</abbr> </p>
+
 </footer>
