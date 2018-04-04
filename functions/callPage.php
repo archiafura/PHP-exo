fe9d93e2b91034d870bd7d82655e0da2adabd74e
Isa Vil <isa.vil@free.fr>
2018-04-04 16:44:36 +0200

    ok

--- functions/callPage.php
+++ functions/callPage.php
@@ -1,7 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Site Dieppe</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href=".\assets\css\main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<div id="container">
+    <?php include "./includes/header.php"; ?>
+    <main>
+<?php 
+callPage();
+?>
+    </main>
+
+    <?php include "./includes/footer.php"; ?> 
+</div>
+</body>
+</html>
+
+
+
 <?php
-function callPage(if (isset($_GET['page'])&& $_GET['page'] != ""){
-    $page = $_GET['page'];
-}
+function callPage() {
+    if (isset($_GET['page'])&& $_GET['page'] != ""){
+        $page = $_GET['page'];
+    }
 
 else {
     $page = "home";
@@ -19,4 +47,4 @@ include $page;
 else{
     include "./includes/home.inc.php";
 }
-)
+}
