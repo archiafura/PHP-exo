87cde5a65b59d0fc9997e15f25ae46dfadb4cba1
Isa Vil <isa.vil@free.fr>
2018-04-06 16:28:38 +0200

    login

---
+++ includes/login.inc.php
@@ -0,0 +1,53 @@
+<h1>Veuillez vous identifier</h1>
+
+<input type="text" value="<?php if(isset($nom)) echo $nom; ?>">Nom</input>
+<input type="text" value="<?php if(isset($prenom)) echo $prenom; ?>" >Prenom</input>
+<input type="password">Mot de passe&nbsp;:</input>
+
+<div>
+    <input type="reset" value="Effacer" />
+    <input type="submit" value="Envoyer" />
+</div>
+
+
+<?php
+if (isset($_POST['frmLogin'])) {
+    if (isset($_POST['mail']) && isset($_POST['mdp'])) {
+        $mail = $_POST['mail'];
+        $mdp = $_POST['mdp'];
+        $mdp = sha1($mdp);
+        $connection = mysqli_connect("localhost", "root", "", "phpdieppe");
+        $requeteVerif = "SELECT * FROM T_USERS
+                        WHERE USEMAIL='$mail'
+                        AND USEPASSWORD='$mdp'";
+                
+        if (!$connection) {
+            die("Erreur MySQL " . mysqli_connect_errno() . " | " . mysqli_connect_error());
+        }
+        else {
+            if ($resultatRequete = mysqli_query($connection, $requeteVerif)) {
+                $nbrResultats = mysqli_num_rows($resultatRequete);
+                if ($nbrResultats > 0) {
+                    $_SESSION['login'] = 1;
+                    
+                }
+                else {
+                    $_SESSION['login'] = 0;
+                    echo "Essaie encore";
+                }
+                mysqli_free_result($resultatRequete);
+            }
+            else {
+                echo "Ach !!! Gros problème de requête !!!";
+            }
+            mysqli_close($connection);
+        }
+    }
+    else {
+        echo "Mmmm, truc qui merdouille dans le formulaire";
+        include "frmLogin.php";        
+    }
+}
+else {
+    include "frmLogin.php";
+}
