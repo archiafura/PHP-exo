e5d8696ad1c18403fb9d928f319bdb4a8560b0d6
Isa Vil <isa.vil@free.fr>
2018-04-06 16:31:29 +0200

    log

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -51,3 +51,24 @@ if (isset($_POST['frmLogin'])) {
 else {
     include "frmLogin.php";
 }
+
+
+/*
+
+<form method="post" action="#">
+    <div>
+        <label for="mail">Login (mail) :</label>
+        <input type="text" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="frmLogin" />
+</form>
+
+*/
